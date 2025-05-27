96044b9f99baa08ba16d65982d726e88fe0522ab
Abderrahim Benmoussa <46120030+wamaw123@users.noreply.github.com>
2025-05-27 22:43:39 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/Chronos-935fec0cb2602b19c2b5270a32dd01f2c50b07e7/', // Add this line
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
